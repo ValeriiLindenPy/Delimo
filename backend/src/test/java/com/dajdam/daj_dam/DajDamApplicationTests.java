faa441f7467c721da577d67c7e0e16e24991bdc9
ValeriiLindenPy <v.linden@mail.ru>
2024-12-25 08:20:54 +0100

    Add: project POM

---
+++ backend/src/test/java/com/dajdam/daj_dam/DajDamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dajdam.daj_dam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DajDamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
