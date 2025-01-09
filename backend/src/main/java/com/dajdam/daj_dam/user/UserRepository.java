2fbd29ad0c492a3b4163797cfb88d8f4ac12b4c1
ValeriiLindenPy <v.linden@mail.ru>
2025-01-09 14:33:16 +0100

    User JPA

--- backend/src/main/java/com/dajdam/daj_dam/user/UserRepository.java
+++ backend/src/main/java/com/dajdam/daj_dam/user/UserRepository.java
@@ -1,17 +1,6 @@
 package com.dajdam.daj_dam.user;
-import java.util.List;
-import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository {
-    List<User> findAll();
 
-    Optional<User> findOne(Long userId);
-
-    User save(User user);
-
-    void delete(Long id);
-
-    void clear();
-
-    User update(User oldUser);
+public interface UserRepository extends JpaRepository<User, Long> {
 }
