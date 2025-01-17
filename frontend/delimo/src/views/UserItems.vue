25f9d9d7cc5d53ca51c98a9abe5ff43bae8de5f9
ValeriiLindenPy <v.linden@mail.ru>
2025-01-17 14:54:13 +0100

    Edit: UserItemCard.vue and UserItems.vue

--- frontend/delimo/src/views/UserItems.vue
+++ frontend/delimo/src/views/UserItems.vue
@@ -1,15 +1,19 @@
 <template>
-  <div class="container flex flex-col justify-center items-center">
-    <h1 class="text-white pt-3 text-2xl font-bold">Moje Stvari</h1>
+  <div class="container">
+    <div class="flex justify-center items-center">
+      <h1 class="text-white pt-3 text-2xl font-bold">Moje Stvari</h1>
+    </div>
+
 
     <div class="mt-2">
-      <PostList :is-editable="true" :posts="posts" />
+      <PostList  v-if="posts" :is-editable="true" :posts="posts" />
     </div>
   </div>
 </template>
 
 <script>
 import ItemList from "@/components/ItemList.vue";
+import apiClient from "@/services/api.js";
 
 export default {
   name: "UserOglasi",
@@ -17,20 +21,16 @@ export default {
 
   data() {
     return {
-      posts: [],
+      posts: null,
     }
   },
 
   created() {
-    // Make sure to convert route param to a number if needed
-    // e.g. const ownerId = Number(this.$route.params.id);
-    // Then filter by that:
-    const ownerId = Number(this.$route.params.id);
-    this.posts = null; //todo
+    apiClient("/my-items").then(res => {
+      console.log(res.data.content);
+      this.posts = res.data.content;
+    })
 
-    if (!this.posts.length) {
-      console.warn('No posts found for this owner id -' + ownerId);
-    }
   },
 }
 </script>
