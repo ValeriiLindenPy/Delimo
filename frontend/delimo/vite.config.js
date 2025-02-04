3241e4dff85f0b34dcdfc13c527c39a1aaa0a3ce
ValeriiLindenPy <v.linden@mail.ru>
2025-02-04 14:39:22 +0100

    Add: FeedBack.vue

--- frontend/delimo/vite.config.js
+++ frontend/delimo/vite.config.js
@@ -4,15 +4,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     // vueDevTools(),
   ],
   // server: {
-  //   host: '192.168.0.16', // Задайте ваш локальный IP-адрес
-  //   port: 5173, // Убедитесь, что порт соответствует вашему
+  //   host: '192.168.0.18',
+  //   port: 5174,
   // },
   resolve: {
     alias: {
