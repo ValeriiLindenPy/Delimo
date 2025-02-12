7cc7466cf25027932167a430e09f14c026c19a0a
ValeriiLindenPy <v.linden@mail.ru>
2025-02-12 11:20:30 +0100

    FIX: security cors

--- backend/src/main/java/rs/delimo/security/SecurityConfig.java
+++ backend/src/main/java/rs/delimo/security/SecurityConfig.java
@@ -89,7 +89,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of(frontendUrl));
+        configuration.setAllowedOrigins(List.of("https://delimo.rs", "https://www.delimo.rs"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
