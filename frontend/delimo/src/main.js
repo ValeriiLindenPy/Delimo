e288d81831e60629b480d59ebeaa1163d7413282
ValeriiLindenPy <v.linden@mail.ru>
2025-02-13 09:27:17 +0100

    Add: Google Analytics

--- frontend/delimo/src/main.js
+++ frontend/delimo/src/main.js
@@ -6,12 +6,18 @@ import App from './App.vue'
 import router from './router'
 import "./assets/tailwind.css"
 import {useAuthStore} from "@/stores/auth.js";
-
+import VueGtag from "vue-gtag";
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.use(VueGtag, {
+    config: { id: "G-BL4CV46VHH" },
+    appName: "Delimo",
+    pageTrackerScreenviewEnabled: true
+}, router);
 
 const authStore = useAuthStore();
 authStore.fetchUser();
